351b092a6b4bf5c8140dd20ccfac187d38831cb4
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-11 00:13:01 +0000

    added disfunctonal code provided on compass lol

--- https_example.js
+++ https_example.js
@@ -1,3 +1,21 @@
 var https = require('https');
 
 console.log('I did it!');
+
+var options = {
+    host: 'www.example.org',
+    path: '/'
+  };
+  
+  // called by https when the request is made.
+  
+  var callback = function(response) {
+    console.log('In response handler callback!');
+    console.log('Response: ', response);
+  }
+  
+  console.log("I'm about to make the request!");
+  
+  https.request(options, callback).end();
+  
+  console.log("I've made the request!");
