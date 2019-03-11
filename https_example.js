8a451ec0b1e88cf7b6609d7c8a0fcf32b51c3d89
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-11 00:20:11 +0000

    so the code was functional, but the first server was not. Monty Python script transferred just fine

--- https_example.js
+++ https_example.js
@@ -3,10 +3,9 @@ var https = require('https');
 console.log('I did it!');
 
 var options = {
-    host: 'www.example.org',
-    path: '/'
+    host: 'stream-large-file.herokuapp.com',
+    path: '/give-me-stuff-now'
   };
-  
   // called by https when the request is made.
   
   var callback = function(response) {
